6eeed92f3368b5293ed4bc3bc0880658eae61303
Hidde Agterberg <hidde8erberg@gmail.com>
2024-11-18 22:53:40 +0100

    added test notebook

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
 
-print(np.random.random())
+x = torch.rand(5, 3)
+print(x)
+
+plt.plot([np.random.random() for _ in range(100)])
+plt.show()
